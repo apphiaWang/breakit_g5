b97a329c88d74d42c1a0425b38e7913ee40170b4
Hardev Khandhar <hardev.khandhar09@gmail.com>
2024-02-20 12:18:48 -0800

    Add navigation menu

---
+++ headers/menu-commands.h
@@ -0,0 +1,30 @@
+#ifndef CMPT785_G5_SECURE_FILESYSTEM_MENU_COMMANDS_H
+#define CMPT785_G5_SECURE_FILESYSTEM_MENU_COMMANDS_H
+
+#include <iostream>
+#include <iomanip>
+
+void available_commands() {
+    int commandWidth = 40;      // Width for the command column
+    int descriptionWidth = 80;  // Width for the description column
+
+    std::cout << std::endl;
+    std::cout << std::setw(commandWidth) << std::left << "Available Commands:" << std::setw(descriptionWidth) << std::left << "Description" << std::endl;
+    std::cout << std::setw(commandWidth) << std::left << "------------------" << std::setw(descriptionWidth) << std::left << "--------------------" << std::endl;
+
+    std::cout << std::setw(commandWidth) << std::left << "cd <directory>" << std::setw(descriptionWidth) << std::left << "Change directory" << std::endl;
+    std::cout << std::setw(commandWidth) << std::left << "pwd" << std::setw(descriptionWidth) << std::left << "Print the current working directory" << std::endl;
+    std::cout << std::setw(commandWidth) << std::left << "ls" << std::setw(descriptionWidth) << std::left << "List the files and directories in the current directory" << std::endl;
+    std::cout << std::setw(commandWidth) << std::left << "cat <filename>" << std::setw(descriptionWidth) << std::left << "Display the content of the given filename" << std::endl;
+
+    std::cout << std::setw(commandWidth) << std::left << "share <filename> <username>" << std::setw(descriptionWidth) << std::left << "Share the file <filename> with the target user <username>" << std::endl;
+    std::cout << std::setw(commandWidth) << std::left << "mkdir <directory>" << std::setw(descriptionWidth) << std::left << "Create a new directory" << std::endl;
+    std::cout << std::setw(commandWidth) << std::left << "mkfile <filename> <contents>" << std::setw(descriptionWidth) << std::left << "Create a new file <filename> with the ascii printable contents <contents>" << std::endl;
+
+    // Assuming the isAdmin check or user role logic is implemented elsewhere
+    std::cout << std::setw(commandWidth) << std::left << "adduser <username>" << std::setw(descriptionWidth) << std::left << "Add new user by the given <username>" << std::endl;
+
+    std::cout << std::setw(commandWidth) << std::left << "exit" << std::setw(descriptionWidth) << std::left << "Terminate the program" << std::endl;
+}
+
+#endif //CMPT785_G5_SECURE_FILESYSTEM_MENU_COMMANDS_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "headers/menu-commands.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::string command;
+    bool shouldExit = false;
+
+    // Authentication function
+    available_commands();
+
+    while (!shouldExit) {
+        std::cout << "\nEnter command: ";
+        std::getline(std::cin, command);
+
+        if (command == "exit") {
+            // terminate function
+            shouldExit = true;
+        } else if (command.substr(0, 3) == "cd ") {
+            //cd function
+        } else if (command == "pwd") {
+            // pwd function
+        } else if (command == "ls") {
+            // ls function
+        } else if (command.substr(0, 4) == "cat ") {
+            // cat function
+        } else {
+            std::cout << "Unknown command. Please try again." << std::endl;
+        }
+    }
+
     return 0;
 }
